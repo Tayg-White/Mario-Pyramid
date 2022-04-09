25d2fdb791ff0d5076c41855effa989893504973
Catawam <90413566+Tayg-White@users.noreply.github.com>
2022-04-09 02:35:48 +0000

    add button + input to determine pyramid height

--- mario.js
+++ mario.js
@@ -14,6 +14,31 @@ printPyramid(height);
  *       #####
  *      ######
  */
+function determineHeightAndThenDrawPyramid(){
+    const element = document.getElementById("drawBtn");
+    element.addEventListener("click", myFunction);
+  
+    for(let i=0; i<height; i++){
+    let bricks = i + 2;
+    let spaces = height - i - 1;
+    let layer = "";
+    for(let j=0; j<spaces; j++){
+      layer += ".";
+    }
+    for(let j=0; j<bricks; j++){
+      layer += "#";
+    }
+    console.log(layer); //once a row is done we print it out
+    // TODO #1
+    // print that pyramid!
+    const pyramid = document.createElement("p");
+    const node = document.createTextNode(layer);
+    pyramid.appendChild(node);
+    const element = document.getElementById("pyramid");
+    element.appendChild(pyramid);
+  }
+}
+
 function printPyramid(height) {
   for(let i=0; i<height; i++){
     let bricks = i + 2;
